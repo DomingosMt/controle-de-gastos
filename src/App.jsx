9fa588b1524d94df4e97eb2031b5c582c26f17d1
DomingosMt <domingosmuratori@outlook.com>
2026-08-03 19:49:54 -0300

    feature/sidebar: html add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div>
+
+    <Sidebar></Sidebar>
       
     </div>
   )
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import { IoAdd } from "react-icons/io5";
+import { MdDashboard, MdOutlineLogout } from "react-icons/md";
+import { PiNewspaperClipping } from "react-icons/pi";
+import { BiBarChartAlt2 } from "react-icons/bi";
+import { GoGear } from "react-icons/go";
+
+function Sidebar() {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+                <div>
+                    <h1>Financer</h1>
+                    <h3>Conta Pessoal</h3>
+                </div>
+                <div>
+                    <IoAdd />
+                    <span>Nova Entrada</span>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <MdDashboard />
+                    <span>Dashboard</span>
+                </div>
+                <div>
+                    <PiNewspaperClipping />
+                    <span>Transações</span>
+                </div>
+                <div>
+                    <BiBarChartAlt2 />
+                    <span>Análise</span>
+                </div>
+                <div>
+                    <GoGear />
+                    <span>Configurações</span>
+                </div>
+            </div>
+
+            <div>
+                <MdOutlineLogout />
+                <span>Sair</span>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
